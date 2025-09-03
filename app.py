21d25ec4873facc2d3ddfd7e2d0215b99ba2ac38
ChoMosigi <whdbsqja1@gmail.com>
2025-09-03 14:32:36 +0900

    tag add

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from bson import ObjectId
 
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY', 'your-secret-key-here')  # 실제 운영에서는 반드시 환경 변수로 관리
+app.secret_key = os.environ.get('SECRET_KEY', 'your-secret-key-here')
 
 # 로그인 체크 데코레이터
 def login_required(f):
@@ -48,20 +48,16 @@ openai = OpenAI(api_key=get_openai_key())
 # MongoDB
 client = MongoClient(os.environ.get("MONGO_URI"))
 db = client.get_database()
+
 # Post Table
 posts = db["posts"]
 users = db["users"]
 
+# 메인화면
 @app.get("/")
 @login_required
 def index():
     return render_template('base.html')
-
-# 예시: 보호된 페이지
-# @app.route('/protected')
-# @login_required
-# def protected_page():
-#     return render_template('protected.html')
 
 @app.get("/db/ping")
 def db_ping():
@@ -71,23 +67,27 @@ def db_ping():
     except Exception as e:
         return jsonify(ok=False, error=str(e)), 500
 
+# 로그인 화면으로 전환
 @app.get("/login")
 def login_page():
     if 'email' in session:
         return redirect(url_for('index'))
     return render_template('login.html')
 
+# 회원가입 화면으로 전환
 @app.get("/signup")
 def signup_page():
     if 'email' in session:
         return redirect(url_for('index'))
     return render_template('signup.html')
 
+# 세션 삭제 및 로그아웃 처리
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for('login_page'))
 
+# 로그인 처리(클라이언트에 json을 받아와 json으로 응답 보냄)
 @app.post("/api/login")
 def login():
     try:
@@ -105,6 +105,7 @@ def login():
     except Exception as e:
         return jsonify({"success": False, "message": "로그인 중 오류가 발생했습니다."})
 
+# 회원가입 처리(클라이언트에 json을 받아와 DB에 데이터 입력 후 json으로 응답 보냄)
 @app.post("/api/signup")
 def signup():
     try:
@@ -132,8 +133,6 @@ def signup():
 def run_task():
     task = add.delay(2, 3)  # 비동기 실행
     return jsonify({"task_id": task.id})
-
-
 
 
 @app.route("/result/<task_id>")
